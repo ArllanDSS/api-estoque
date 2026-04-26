ed6e7d94de5fd5531b99e902426ea895e6694ac0
ArllaN <arllandavid@gmail.com>
2026-04-26 03:03:45 -0300

    feat: criado ProdutoController

---
+++ src/main/java/br/com/arllan/estoque/controller/ProdutoController.java
@@ -0,0 +1,33 @@
+package br.com.arllan.estoque.controller;
+
+import br.com.arllan.estoque.model.Produto;
+import br.com.arllan.estoque.service.ProdutoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    @GetMapping
+    public List<Produto> listar() {
+        return produtoService.listar();
+    }
+
+    @PostMapping
+    public Produto criar(@RequestBody Produto produto) {
+        return produtoService.criar(
+                produto.getNome(),
+                produto.getDescricao(),
+                produto.getPreco(),
+                produto.getQuantidadeEstoque()
+        );
+    }
+}
